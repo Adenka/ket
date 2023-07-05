b351969bcee909abc8a7ee9711eb5bc48eaf4622
jb438249 <jb438249@mimuw.edu.pl>
2023-07-05 12:17:30 +0200

    UI drawer fix

--- set/src/components/startPage/startPageDrawer.js
+++ set/src/components/startPage/startPageDrawer.js
@@ -1,10 +1,14 @@
-import { Divider, Drawer, IconButton, Typography } from "@mui/material";
+import { Drawer } from "@mui/material";
 import React from "react";
-import PersonIcon from '@mui/icons-material/Person';
 import { useTheme } from "@emotion/react";
 import { grey } from "@mui/material/colors";
 import { useNavigate } from "react-router-dom";
 import { makeStyles } from "@mui/styles";
+import {    BigIconButton,
+            BigPersonIcon,
+            UsernameTypography,
+            DrawerDivider,
+            DrawerElement } from "./startPageDrawerComponents";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -12,37 +16,6 @@ const useStyles = makeStyles(theme => ({
         backgroundColor: theme.palette.secondary.light,
         color: grey[800],
         height: "100%"
-    },
-    drawerTitle: {
-        padding: "2.5rem",
-        paddingLeft: 0,
-        fontFamily: "Righteous",
-        letterSpacing: "0.125rem",
-        fontSize: 32
-    },
-    userIconWrapper: {
-        width: 64,
-        height: 64,
-        margin: "2rem 1rem 2rem 1rem"
-    },
-    userIcon: {
-        color: grey[800],
-        fontSize: 40
-    },
-    divider: {
-        margin: "0 1rem 0 1rem",
-        backgroundColor: theme.palette.secondary.main
-    },
-    drawerListElement: {
-        padding: "2rem",
-        transition: "0.25s",
-        color: grey[600],
-        fontFamily: "Prompt",
-        fontSize: 22.5,
-        "&:hover": {
-            color: theme.palette.secondary.light,
-            backgroundColor: theme.palette.primary.main
-        }
     }
 }))
 
@@ -59,20 +32,19 @@ const StartPageDrawer = ({openDrawer, setOpenDrawer}) => {
     >
         <div className={classes.root}>
             <div style={{display: "flex"}}>
-                <IconButton className={classes.userIconWrapper} disabled>
-                    <PersonIcon className={classes.userIcon}/>
-                </IconButton>
-                <Typography className={classes.drawerTitle}>
+                <BigIconButton disabled>
+                    <BigPersonIcon/>
+                </BigIconButton>
+                <UsernameTypography>
                     {localStorage.getItem("username")}
-                </Typography>
+                </UsernameTypography>
             </div>
-            <Divider className={classes.divider}/>
-            <Typography
-                className = {classes.drawerListElement}
+            <DrawerDivider/>
+            <DrawerElement
                 onClick = {() => {navigate("/scores")}}
             >
                 Mah scorez
-            </Typography>
+            </DrawerElement>
         </div>
     </Drawer>
 }
---
+++ set/src/components/startPage/startPageDrawerComponents.js
@@ -0,0 +1,76 @@
+import React from "react";
+import PersonIcon from '@mui/icons-material/Person';
+import { Divider, IconButton, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+
+const BigIconButton = (props) => (
+    <IconButton
+        sx = {{
+            width: 64,
+            height: 64,
+            margin: "2rem 1rem 2rem 1rem"
+        }}
+    >
+        {props.children}
+    </IconButton>
+)
+
+const BigPersonIcon = (props) => (
+    <PersonIcon
+        sx = {{
+            color: grey[800],
+            fontSize: 40
+        }}
+    />
+)
+
+const UsernameTypography = (props) => (
+    <Typography
+        sx = {{
+            padding: "2.5rem",
+            paddingLeft: 0,
+            fontFamily: "Righteous",
+            letterSpacing: "0.125rem",
+            fontSize: 32
+        }}
+    >
+        {props.children}
+    </Typography>
+)
+
+const DrawerDivider = (props) => (
+    <Divider
+        sx = {{
+            margin: "0 1rem 0 1rem",
+            backgroundColor: (theme) => theme.palette.secondary.main
+        }}
+        {...props}
+    />
+)
+
+const DrawerElement = (props) => (
+    <Typography
+        sx = {{
+            padding: "2rem",
+            transition: "0.25s",
+            color: grey[600],
+            fontFamily: "Prompt",
+            fontSize: 22.5,
+            "&:hover": {
+                color: (theme) => theme.palette.secondary.light,
+                backgroundColor: (theme) => theme.palette.primary.main
+            }
+        }}
+        {...props}
+    >
+        {props.children}
+    </Typography>
+)
+
+export {
+    BigIconButton,
+    BigPersonIcon,
+    UsernameTypography,
+    DrawerDivider,
+    DrawerElement
+}
